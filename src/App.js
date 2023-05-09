7b2c445bd96c71eccf93c45a9871b0469bb42187
SantoshChapagai <santoshchapagai100@gmail.com>
2023-05-09 21:29:56 +0300

    adding navigation between pages

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Recipies from './pages/Recipies';
 import AddRecipe from './pages/AddRecipie';
+import Header from './Components/Header';
 const App = () => {
   return (
     <div className='app'>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/recipies' element={<Recipies />} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import Header from '../Components/Header';
+import { NavLink } from 'react-router-dom';
+import './styles.css'
+
 
 const Home = () => {
   return (
     <div>
-      <div>
-        <Header />
+      <div className='home'>
+        <h1>TasteIT</h1>
+        <p>TasteIT is a recipie app which allows to add and browse recipes</p>
+        <NavLink to='/recipies'>Browse recipes</NavLink>
       </div>
-      Home will be here
+      <div>
+        <h2>Looking for recipes</h2>
+      </div>
+      <div className='home_cards'>
+        <div className='card'>
+          <h4>Browse recipes</h4>
+          <p>Find the Awesome recipe across globes</p>
+        </div>
+        <div className='card'>
+          <h4>Add recipe</h4>
+          <p>Add recipe from your country</p>
+        </div>
+        <div className='card'></div>
+
+      </div>
+
     </div>
   );
 };
