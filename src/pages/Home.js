05769e02b8b6955b76eabf4383328a69fe7f091c
SantoshChapagai <santoshchapagai100@gmail.com>
2023-05-14 16:53:09 +0300

    updating css for the home page video section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
     <div>
       <div className='home'>
         <video autoPlay loop muted play-inline id="background_video">
-          <source src="https://player.vimeo.com/external/365129177.sd.mp4?s=14d315c4858f8cf7778befff7069056e0b4de516&profile_id=164&oauth2_token_id=57447761" type="video/mp4" />
+          <source src="https://mazwai.com/videvo_files/video/free/2020-01/small_watermarked/200116_Lens%20effect_4k_072_preview.webm" type="video/mp4" />
         </video>
         {/* <div>
           <video autoPlay loop muted play-inline id="background_video">
@@ -16,9 +16,11 @@ const Home = () => {
           </video>
         </div> */}
         <div className='container'>
-          <h1>TasteIT</h1>
-          <p>TasteIT is a recipe app which allows to add and browse recipes</p>
-          <NavLink to='/recipes'><button>Browse recipes</button></NavLink>
+          <div className='list_holder'>
+            <h1>TasteIT</h1>
+            <p>TasteIT is a recipe app which allows to add and browse recipes</p>
+            <NavLink to='/recipes'><button>Browse recipes</button></NavLink>
+          </div>
         </div>
       </div>
       <div>
