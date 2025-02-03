f192d17b9031dcbfb27eb02c37835e75286d995b
SantoshChapagai <you@example.com>
2025-02-03 18:08:58 +0100

    update the style of the receipe application

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,41 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Card from '../UI/Card';
-import './css/styles.css';
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "../UI/Card";
+import "./css/styles.css";
 
 const Recipes = () => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [searchInput, setSearchInput] = useState('');
+  const [searchInput, setSearchInput] = useState("");
   useEffect(() => {
     setIsLoading(true);
-    axios.get("http://localhost:4000/recipes")
-      .then(res => {
-        setData(res.data);
-        setIsLoading(false);
-      });
+    axios.get("http://localhost:4000/recipes").then((res) => {
+      setData(res.data);
+      setIsLoading(false);
+    });
   }, []);
   const searchInputHandler = (e) => {
-    setSearchInput(e.target.value)
-  }
-  const searchFilter = data.filter(recipes => {
+    setSearchInput(e.target.value);
+  };
+  const searchFilter = data.filter((recipes) => {
     return recipes.name?.toLowerCase().includes(searchInput.toLowerCase());
-  })
+  });
 
   if (isLoading) {
-    return <p>Loading...</p>
+    return <p>Loading...</p>;
   }
   return (
-    <div className='recipes'>
-      <div className='search'>
-        <input onChange={searchInputHandler} />
+    <div className="recipes">
+      <div className="search">
+        <input onChange={searchInputHandler} className="input_style" />
       </div>
-      <div className='all_card'>
+      <div className="all_card">
         {searchFilter.map((data) => (
-          <Card key={data.id}
-            recipe={data}
-          />
+          <Card key={data.id} recipe={data} />
         ))}
       </div>
     </div>
