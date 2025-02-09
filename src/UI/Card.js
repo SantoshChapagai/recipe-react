ef2a959ff51e01056e4e8d18f73de9f64266ab13
SantoshChapagai <you@example.com>
2025-02-09 20:31:16 +0200

    initiate the adding recipe to the favourite list

--- src/UI/Card.js
+++ src/UI/Card.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Flags from '../Components/Flags';
-import './card.css'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Flags from "../Components/Flags";
+import "./card.css";
 
 const Card = ({ recipe }) => {
+  const [isFavourite, setIsFavourite] = useState(false);
   return (
-
-    <div className='card_container'>
-      <div className='flag'>
+    <div className="card_container">
+      <div>
+        
+      </div>
+      <div className="flag">
         <Flags recipe={recipe} />
       </div>
       <img src={recipe.image} alt={recipe.name} />
       <h3>{recipe.name}</h3>
-      <div className='more'>
+      <div className="more">
         <Link to={`/recipes/${recipe.id}`}>See more</Link>
       </div>
-    </div >
+    </div>
   );
 };
 
